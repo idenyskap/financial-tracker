676daebfd5acf16ffa7944dea16aaea74e2720fb
idenyskap <business.kapralov@gmail.com>
2026-04-07 12:47:59 +0300

    Replace Caffeine cache with Redis

--- src/main/java/com/example/financial_tracker/config/CacheConfig.java
+++ src/main/java/com/example/financial_tracker/config/CacheConfig.java
@@ -1,23 +1,30 @@
 package com.example.financial_tracker.config;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
-  @Bean
-  public CacheManager cacheManager() {
-      CaffeineCacheManager cacheManager = new CaffeineCacheManager("exchangeRates");
-      cacheManager.setCaffeine(Caffeine.newBuilder()
-              .maximumSize(500)
-              .expireAfterWrite(1, TimeUnit.HOURS));
-      return cacheManager;
-  }
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(1))
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair
+                                .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(config)
+                .build();
+    }
 }
